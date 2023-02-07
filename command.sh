d22a154a69c290c82a1036aa3ff93dc9c73e93b0
zoff99 <zoff99@users.noreply.github.com>
2023-02-07 18:01:13 +0100

    tweak command

--- command.sh
+++ command.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
-# rsstail -p -P -i 3 -H -u https://github.com/openwrt/openwrt/releases.atom -n 2 -
-rsstail -p -P -i 3 -H -u https://www.ots.at/rss/index -n 0 -
-# tail -f /var/log/kern.log
-
+# rsstail -p -P -i 3600 -H -u https://github.com/openwrt/openwrt/releases.atom -n 0 -
+rsstail -p -P -i 122 -H -u https://www.ots.at/rss/index -n 0 -
